cc9b7bc9e2db91ea153e2901c81e646bad37e1f6
Eugene Mitikari <accakyra@inbox.ru>
2020-05-17 17:33:04 +0700

    refactor main structure

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import LoginPage from "./components/LoginPage";
 import MainPage from "./components/MainPage";
-
 import "tabler-react/dist/Tabler.css";
 
-function App() {
+type Props = {||};
+
+function App(props: Props): React.Node {
     return (
-        <Router>
-            <Switch>
-                <Route path="/login" exact={true} component={LoginPage}/>
-                <Route path="/" component={MainPage}/>
-            </Switch>
-        </Router>
+        <React.StrictMode>
+            <Router>
+                <Switch>
+                    <Route exact path="/login" component={LoginPage}/>
+                    <Route path="/" component={MainPage}/>
+                </Switch>
+            </Router>
+        </React.StrictMode>
     );
 }
 
