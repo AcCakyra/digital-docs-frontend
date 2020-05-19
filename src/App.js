24b70c43a050df7632d81339719aaff38728cb8b
Eugene Mitikari <accakyra@inbox.ru>
2020-05-19 23:24:38 +0700

    add forgot password page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import LoginPage from "./components/LoginPage";
 import MainPage from "./components/MainPage";
+import ForgotPassword from "./components/ForgotPassword";
+
 import "tabler-react/dist/Tabler.css";
 
 type Props = {||};
@@ -12,6 +14,7 @@ function App(props: Props): React.Node {
             <Router>
                 <Switch>
                     <Route exact path="/login" component={LoginPage}/>
+                    <Route exact path="/forgot_password" component={ForgotPassword}/>
                     <Route path="/" component={MainPage}/>
                 </Switch>
             </Router>
---
+++ src/components/ForgotPassword.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ForgotPassword extends React.Component {
+
+
+    render(): React.ReactNode {
+
+        return (
+            <div>
+                Hi
+            </div>
+        )
+    };
+}
+
+export default ForgotPassword;
