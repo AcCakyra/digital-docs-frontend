caa55f898296907386d28fdbb8cf1f1bb0809476
Eugene Mitikari <accakyra@inbox.ru>
2020-05-19 23:24:24 +0700

    change login form

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,145 +1,111 @@
 import React from "react";
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import {withStyles} from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import {Alert} from '@material-ui/lab';
-import Collapse from '@material-ui/core/Collapse';
-
 import AuthenticationService from "./../services/AuthenticationService";
 
-const styles = theme => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-    error: {
-        marginTop: theme.spacing(1)
-    }
-});
+import {
+    Card, Form, Button, Grid, Page, Alert,
+} from "tabler-react";
 
 class LoginPage extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            email: "",
-            password: "",
-            loginError: false
+            error: "",
         };
-    }
-
-    handleEmailChange = (e) => {
-        this.setState({
-            email: e.target.value
-        })
-    }
-
-    handlePasswordChange = (e) => {
-        this.setState({
-            password: e.target.value
-        })
     }
 
     login = async (e) => {
         e.preventDefault();
-        if (this.state.email === '' || this.state.password === '') {
+        this.setState({
+            invalidEmail: false
+        })
+
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        if (email === '' || password === '') {
             return
         }
-        await AuthenticationService.login(this.state.email, this.state.password).then(r => {
+
+        if (!this.validateEmail(email)) {
+            this.setState({
+                error: "Некорректный e-mail"
+            })
+            return
+        }
+
+        await AuthenticationService.login(email, password).then(r => {
             this.props.history.push('/');
         }).catch(() => {
             this.setState({
-                loginError: true
+                error: "Неверный e-mail или пароль"
             })
         });
     }
 
-    render() {
-        const {classes} = this.props;
+    validateEmail = (email) => {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    forgotPassword = () => {
+        this.props.history.push('/forgot_password');
+    }
+
+    render(): React.ReactNode {
 
         return (
-            <Container component="main" maxWidth="xs">
-                <CssBaseline/>
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon/>
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Войти в свой аккаунт
-                    </Typography>
-                    <Collapse className={classes.error} in={this.state.loginError}>
-                        <Alert severity="error">
-                            Неверный e-mail или пароль
-                        </Alert>
-                    </Collapse>
-                    <form className={classes.form} noValidate>
-                        <TextField
-                            variant="standard"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            value={this.state.email}
-                            onChange={this.handleEmailChange}
-                        />
-                        <TextField
-                            variant="standard"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            value={this.state.password}
-                            onChange={this.handlePasswordChange}
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                            onClick={this.login}
-                        >
-                            Войти
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Забыли пароль?
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </form>
-                </div>
-            </Container>
+            <Page.Content className="page">
+                <Grid.Row cards={true}>
+                    <Grid.Col lg={5} md={7} sm={8} xs={9} className="mx-auto">
+                        {this.state.error !== "" ?
+                            <Alert type="danger" icon="alert-triangle">
+                                {this.state.error}
+                            </Alert>
+                            :
+                            null
+                        }
+                        <Card>
+                            <Card.Header>
+                                <Card.Title>
+                                    Войти в аккаунт
+                                </Card.Title>
+                            </Card.Header>
+                            <Card.Body>
+                                <Form onSubmit={this.login}>
+                                    <Form.Group label="Email">
+                                        <Form.Input
+                                            name="email"
+                                            placeholder="Email"
+                                            autoComplete="email"
+                                        />
+                                    </Form.Group>
+                                    <Form.Group label="Пароль">
+                                        <Form.Input
+                                            name="password"
+                                            placeholder="Пароль"
+                                            type="password"
+                                        />
+                                    </Form.Group>
+                                    <Form.Footer>
+                                        <Button
+                                            type="submit"
+                                            color="primary" block>
+                                            Войти
+                                        </Button>
+                                    </Form.Footer>
+                                </Form>
+                            </Card.Body>
+                            <Button color="secondary"
+                                    onClick={this.forgotPassword}>
+                                Забыли пароль?
+                            </Button>
+                        </Card>
+                    </Grid.Col>
+                </Grid.Row>
+            </Page.Content>
         )
-    }
+    };
 }
 
-export default withStyles(styles)(LoginPage);
+export default LoginPage;
